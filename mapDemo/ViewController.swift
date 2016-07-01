ac395cc83d3c5b2a7c3cfef9a2e5b3ac3c9cb971
kaylant <kaylant@gmail.com>
2016-06-30 22:31:34 -0500

    added pin

--- mapDemo/ViewController.swift
+++ mapDemo/ViewController.swift
@@ -40,6 +40,58 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         map.setRegion(region, animated: true)
         
+        // for a pin
+        var annotation = MKPointAnnotation()
+        
+        // set coordinate to location as used earlier
+        annotation.coordinate = location
+    
+        // title of annotation
+        annotation.title = "Brooklyn"
+        
+        annotation.subtitle = "Home of Wythe Hotel"
+        
+        // add the annotation to the map
+        map.addAnnotation(annotation)
+        
+        // for user to add their own pin, 
+        // ui long press gesture recognizer
+        // "action" is name of function to be run when user does a long press
+        // : means gestureRecognizer will be sent
+        var uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        
+        // number of seconds user has to hold the press
+        uilpgr.minimumPressDuration = 2
+        
+        // add to the map, like before
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    // action function, runs when long press is recognized
+    // receives variable gestureRecognizer
+    func action(gestureRecognizer:UIGestureRecognizer) {
+        
+        print("Gesture Recognized")
+        
+        // gives us the point user has pressed RELATIVE to the map in the view 
+        // self refers to view controller
+        var touchPoint = gestureRecognizer.locationInView(self.map)
+        
+        var newCoordinate: CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+        
+        var annotation = MKPointAnnotation()
+        
+        annotation.coordinate = newCoordinate
+        
+        // title of annotation
+        annotation.title = "New Place"
+        
+        annotation.subtitle = "To Visit One Day"
+        
+        // add the annotation to the map
+        map.addAnnotation(annotation)
+        
     }
 
     override func didReceiveMemoryWarning() {
